cecc8a759c2ac14f052c0a823746058159889953
Anna Chiara Melioli <138586939+meliolanna@users.noreply.github.com>
2025-01-09 17:28:02 +0100

    Update FCR.sh

--- 24nov6/FCR.sh
+++ 24nov6/FCR.sh
@@ -4,18 +4,20 @@ cd $1
 
 livello=`expr $3 + 1`
 conteggio=0
-
+#io ho fatto il file tmp ma la prof voleva la stringa mi sa
+files=
 #controllo se la gerarchia è al livello giusto
 if test $2 -eq livello
 then 
   #se si stampo e vedo se ha due file
   echo Trovata directory al livello $2: `pwd`
-  for i in *
+  for F in *
   do
-    if test -f $i
+    if test -f $F
     then
       conteggio=`expr conteggio + 1`
-      $i >> $4
+      #$i >> $4
+      files="$files $F"
     fi
   done
   #se sono almeno 2 file chiamo c
@@ -23,12 +25,12 @@ then
   then
     #chiamo c
     echo Invoco la parte in c
-    ./main `pwd` `cat $4`
+    ./main $1 $files #`cat $4`
   else
     echo Non è stato possibile invocare la parte c.
   fi
-    
-else
+fi
+
   #se no cerco le directory in cui posso continuare a cercare
   for i in *
   do
@@ -37,4 +39,4 @@ else
       sh $0 $1 $2 $livello #invoco il programma con 0 e i parametri
     fi
   done
-fi
+
